909d94192c71c01e2912d2a7bc98abaeb4726f8c
Matt Lee <matt@bymattlee.com>
2021-01-23 11:29:14 -0800

    Clean up image srcset formatting

--- components/ImageItem.vue
+++ components/ImageItem.vue
@@ -14,10 +14,11 @@ export default {
     imageSrcset() {
       const imageWidths = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800]
       const srcset = []
+
       imageWidths.forEach((width) => {
-        srcset.push(`
-          ${this.$urlFor(this.assetId).width(width).url()} ${width}w
-        `)
+        srcset.push(
+          `${this.$urlFor(this.assetId).width(width).url()} ${width}w`
+        )
       })
 
       return srcset.join(', ')
